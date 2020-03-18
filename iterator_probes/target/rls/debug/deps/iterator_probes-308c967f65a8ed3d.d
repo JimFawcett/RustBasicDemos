8c2005efd902871fad2ac51aa0a17a2eb4360033
unknown <jfawcett@twcny.rr.com>
2020-03-18 08:42:52 -0400

    rebuilt

---
+++ iterator_probes/target/rls/debug/deps/iterator_probes-308c967f65a8ed3d.d
@@ -0,0 +1,5 @@
+c:\github\JimFawcett\RustBasicDemos\iterator_probes\target\rls\debug\deps\iterator_probes-308c967f65a8ed3d.rmeta: src\main.rs
+
+c:\github\JimFawcett\RustBasicDemos\iterator_probes\target\rls\debug\deps\iterator_probes-308c967f65a8ed3d.d: src\main.rs
+
+src\main.rs:
